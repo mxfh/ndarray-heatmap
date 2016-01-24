94e86956a3b2ae874c82d62ceb019b034e18d1ce
akngs <jania902@gmail.com>
2016-01-24 22:07:54 +0900

    Simplify comments

--- test/unit/ndarray-heatmap.js
+++ test/unit/ndarray-heatmap.js
@@ -10,28 +10,28 @@ describe('heatmap', () => {
   });
 
   it('should provide data accessor', () => {
-    // Check default value
+    // default value
     expect(unpack(hm.data())).to.deep.equal([[0]]);
 
-    // Check setter and getter
+    // setter and getter
     expect(hm.data(pack([[1, 2], [3, 4]]))).to.equal(hm);
     expect(unpack(hm.data())).to.deep.equal([[1, 2], [3, 4]]);
   });
 
   it('should provide colorSteps accessor', () => {
-    // Check default value
+    // default value
     expect(hm.colorSteps()).to.equal(256);
 
-    // Check setter and getter
+    // setter and getter
     expect(hm.colorSteps(10)).to.equal(hm);
     expect(hm.colorSteps()).to.equal(10);
   });
 
   it('should provide colorRange accessor', () => {
-    // Check default value
+    // default value
     expect(hm.colorRange()).to.deep.equal(['#000000', '#FFFFFF']);
 
-    // Check setter and getter
+    // setter and getter
     expect(hm.colorRange(['#FF0000', '#00FFFF'])).to.equal(hm);
     expect(hm.colorRange()).to.deep.equal(['#FF0000', '#00FFFF']);
   });
